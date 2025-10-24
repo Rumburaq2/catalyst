6a74e3fbbf4d019f76dd2db43d9a5ba0a6d97dd8
Rumburaq2 <oliver.hurt@student.gyarab.cz>
2025-10-24 17:12:52 +0200

    add: edit default assignticket action to send the info about newly created ticket to outside http server

--- app/data/scripts/assigntickets.py
+++ app/data/scripts/assigntickets.py
@@ -2,11 +2,22 @@ import sys
 import json
 import random
 import os
-
 import requests
 
 # Parse the ticket from the input
 ticket = json.loads(sys.argv[1])
+
+# Send the ticket data to your local HTTP server
+try:
+    response = requests.post(
+        "http://127.0.0.1:8001",
+        json=ticket,
+        timeout=5
+    )
+    response.raise_for_status()
+    print("Sent ticket info to server successfully:", response.json())
+except Exception as e:
+    print("Failed to send ticket info to server:", e)
 
 url = os.environ["CATALYST_APP_URL"]
 header = {"Authorization": "Bearer " + os.environ["CATALYST_TOKEN"]}
