75c80b385ba5074a1e3232a61a25535e46e6453c
Rumburaq2 <oliver.hurt@student.gyarab.cz>
2025-11-02 15:21:42 +0100

    add: edit default assignticket action to parse incoming severity and give it to the newly created ticket.

--- app/data/scripts/assigntickets.py
+++ app/data/scripts/assigntickets.py
@@ -25,8 +25,13 @@ header = {"Authorization": "Bearer " + os.environ["CATALYST_TOKEN"]}
 # Get a random user
 users = requests.get(url + "/api/users", headers=header).json()
 random_user = random.choice(users)
+#Get Severity
+severity = ticket["record"]["state"]["severity"]
 
 # Assign the ticket to the random user
 requests.patch(url + "/api/tickets/" + ticket["record"]["id"], headers=header, json={
-    "owner": random_user["id"]
+    "owner": random_user["id"],
+    "state": {
+        "severity": severity
+    }
 })
