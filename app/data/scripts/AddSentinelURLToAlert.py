b11ae75358642c87d8765b1286ef010caee3a1fa
Rumburaq2 <oliver.hurt@student.gyarab.cz>
2026-02-25 21:52:05 +0100

    Update AddSentinelURLToAlert.py

--- app/data/scripts/AddSentinelURLToAlert.py
+++ app/data/scripts/AddSentinelURLToAlert.py
@@ -17,6 +17,34 @@ header = {"Authorization": "Bearer " + os.environ["CATALYST_TOKEN"]}
 event = json.loads(sys.argv[1])
 body = json.loads(event["body"])
 
+# Extract the existing state or create an empty dictionary
+ticket_state = body.get("state", {})
+
+# Add custom ARM_id to the state dictionary
+ticket_state["ARM_id"] = body.get("IncidentARM_ID")
+ticket_state["severity"] = body.get("IncidentSev")
+
+# Get data for alert creation
+ticket_payload = {
+    "state": ticket_state
+}
+
+ticketID = body.get("ticketID")
+
+#link_payload = {
+#    "ARM_id": body.get("IncidentARM_ID"),
+#}
+
+patch_response = requests.patch(
+    url + "/api/tickets/" + ticketID,
+    headers=header,
+    json=ticket_payload
+)
+
+response_data = patch_response.json()
+
+#requests.post("https://webhook.site/c84ee3e3-04bd-4626-aa29-27844c8b14b3", #json=response_data)
+
 # Create a Link
 link_payload = {
     "ticket": body.get("ticketID"),
